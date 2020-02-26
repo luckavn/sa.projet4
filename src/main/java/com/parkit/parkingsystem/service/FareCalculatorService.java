2102e6b8878f87f81912395375f9e01dca22b05a
lucka <lucas.vannier@axa.fr>
2020-02-26 12:07:20 +0100

    fix code in order to calculate right fees, revising more than 24 hours duration

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -2,6 +2,10 @@ package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
+import org.joda.time.DateTime;
+
+import java.text.DecimalFormat;
+import java.util.Date;
 
 public class FareCalculatorService {
 
@@ -10,37 +14,20 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inDay = ticket.getInTime().getDay();
-        int outDay = ticket.getOutTime().getDay();
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-        int inMinutes  = ticket.getInTime().getMinutes();
-        int outMinutes  = ticket.getOutTime().getMinutes();
-        int minutesDuration;
-
-        if (inMinutes > outMinutes) {
-            minutesDuration = (60 - inMinutes) + outMinutes;
-        } else {
-            minutesDuration = outMinutes - inMinutes;
-        }
-
-        int hourDurationInMinutes = (outHour - inHour) * 60;
-        int dayInMinutes = ((outDay - inDay) * 24) * 60;
-        int totalDuration = minutesDuration + hourDurationInMinutes + dayInMinutes;
+        DecimalFormat formatter = new DecimalFormat("###,###");
+        long diff = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
+        double diffMin = (double) (diff / (60*1000));
+        double duration = diffMin / 60;
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                double calculatingTicketPrice = totalDuration * Fare.CAR_RATE_PER_MINUTES;
-                double ticketPrice =  Math.round(calculatingTicketPrice * (1.0/0.01)) / (1.0/0.01);
-                ticket.setPrice(ticketPrice);
+                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
-                double calculatingTicketPrice = totalDuration * Fare.BIKE_RATE_PER_MINUTES;
-                double ticketPrice =  Math.round(calculatingTicketPrice * (1.0/0.01)) / (1.0/0.01);
-                ticket.setPrice(ticketPrice);
+                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
