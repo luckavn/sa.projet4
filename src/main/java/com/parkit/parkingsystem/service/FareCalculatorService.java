608cebcfa5172a1b4d8635e62575d8d7dc36d92a
lucka <lucas.vannier@axa.fr>
2020-03-06 16:17:55 +0100

    Revising FareCalculatorService in order to reduce price by 5% and format price to two decimal

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,9 +3,12 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.util.InputReaderUtil;
 
 public class FareCalculatorService {
+    InputReaderUtil inputReaderUtil = new InputReaderUtil();
     TicketDAO ticketDAO = new TicketDAO();
+
 
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
@@ -18,18 +21,36 @@ public class FareCalculatorService {
         //TODO: Some tests are failing here. Need to check if this logic is correct
         int duration = outHour - inHour;
 
-        switch (ticket.getParkingSpot().getParkingType()){
+        switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                double Price = (duration * Fare.CAR_RATE_PER_HOUR);
+
+                boolean regularCustomer = ticketDAO.getHistory(ticket.getVehicleRegNumber());
+                if (regularCustomer) {
+                    Price = inputReaderUtil.applyFivePourcentOff(Price);
+                }
+
+                Price = inputReaderUtil.formatToTwoDecimal(Price);
+                ticket.setPrice(Price);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                double Price = (duration * Fare.BIKE_RATE_PER_HOUR);
+
+                boolean regularCustomer = ticketDAO.getHistory(ticket.getVehicleRegNumber());
+                if (regularCustomer) {
+                    Price = inputReaderUtil.applyFivePourcentOff(Price);
+                }
+
+                Price = inputReaderUtil.formatToTwoDecimal(Price);
+                ticket.setPrice(Price);
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default:
+                throw new IllegalArgumentException("Unknown Parking Type");
         }
     }
+
     public void setTicketDAO(TicketDAO ticketDAO) {
         this.ticketDAO = ticketDAO;
     }
