156536fffc23356f4dcaec0424c4d8e91a44e986
luckavn <60694530+luckavn@users.noreply.github.com>
2020-03-13 15:11:53 +0100

    Update DataBaseConfigTest.java

--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseConfigTest.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseConfigTest.java
@@ -5,15 +5,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
+import java.util.ResourceBundle;
 
 public class DataBaseConfigTest extends DataBaseConfig {
     private static final Logger logger = LogManager.getLogger("DataBaseConfigTest");
+    ResourceBundle myBundle = ResourceBundle.getBundle("database");
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/test?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=Europe/Paris", "root", "rootroot");
+                myBundle.getString("db.url.test"), myBundle.getString("db.user"), myBundle.getString("db.pwd"));
     }
 
     public void closeConnection(Connection con) {
